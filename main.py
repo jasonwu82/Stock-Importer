7e6b93da7679c2d8d759e1ad9023c5b8ad85834b
jasonwu82 <jasonwu082@gmail.com>
2021-02-20 18:08:02 -0800

    other field

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
+from pandas import DataFrame
 
 
 def descriptionToBuySellCategory(description: str):
-    print(description)
     if description.startswith('Bought'):
         return 'Buy'
     elif description.startswith('Sold'):
@@ -11,14 +11,26 @@ def descriptionToBuySellCategory(description: str):
         return 'Other'
 
 
-def generateBuySellCategory(df):
-    df['type'] = df['DESCRIPTION'].apply(descriptionToBuySellCategory)
+def generateBuySellCategory(df: DataFrame) -> DataFrame:
+    df['Type'] = df['DESCRIPTION'].apply(descriptionToBuySellCategory)
     return df
 
 
+def projectToTracker(df: DataFrame):
+    df = generateBuySellCategory(df)
+    df['Date'] = df['DATE']
+    df['Stock'] = df['SYMBOL']
+    df['Transacted Units'] = df['QUANTITY']
+    df['Transacted Price (per unit)'] = df['PRICE']
+    df = df[df['Type'] != 'Other']
+    return df[['Date', 'Type', 'Stock', 'Transacted Units', 'Transacted Price (per unit)']]
+
+
 if __name__ == '__main__':
-    data = pd.read_csv('inputs/transactions.csv', skipfooter=1)
-    data = generateBuySellCategory(data)
-    print(data.head())
+    df = pd.read_csv('inputs/transactions.csv', skipfooter=1)
+    df = df[['DATE', 'SYMBOL', 'DESCRIPTION', 'QUANTITY', 'PRICE', 'AMOUNT']]
+    df = projectToTracker(df)
+    df.to_csv('outputs/output_transactions.csv', index=False, header=False)
+    print(df.head())
 
 
